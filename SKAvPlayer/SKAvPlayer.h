0a0d442d1129484fa74940c06b5887c2ecc30fd4
Shin-Kai Chen <skchen@sixnology.com>
2016-04-25 10:39:49 +0800

    Add project files

---
+++ SKAvPlayer/SKAvPlayer.h
@@ -0,0 +1,19 @@
+//
+//  SKAvPlayer.h
+//  SKAvPlayer
+//
+//  Created by Shin-Kai Chen on 2016/4/25.
+//  Copyright © 2016年 SK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKAvPlayer.
+FOUNDATION_EXPORT double SKAvPlayerVersionNumber;
+
+//! Project version string for SKAvPlayer.
+FOUNDATION_EXPORT const unsigned char SKAvPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKAvPlayer/PublicHeader.h>
+
+
